ab75098d1f075186136ce30e0067eed57595403b
Lukas Uhl <lukasuhl@Mac-mini.fritz.box>
2026-03-22 06:53:28 +0100

    feat: Vercel Analytics integriert

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
@@ -9,6 +10,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://uhl-systems.com',
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
   integrations: [mdx(), sitemap()],
 
   vite: {
